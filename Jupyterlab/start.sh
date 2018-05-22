8e850f27f4ed79106efba6cbe0ace70340e6d598
ChuckRIHead <chuckhead@gmail.com>
2018-05-22 15:26:48 -0700

    reverting to working version from Rasa env

--- Jupyterlab/start.sh
+++ Jupyterlab/start.sh
@@ -11,12 +11,12 @@ cat >> $CONF_FILE << EOF
 c = get_config()
 c.NotebookApp.notebook_dir = '${DRT_WORKING_DIR:-"/"}'
 c.NotebookApp.base_url = '${PREFIX}'
+c.NotebookApp.base_kernel_url = '${PREFIX}'
+c.NotebookApp.base_project_url = '${PREFIX}'
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}, 'static_url_prefix': '${PREFIX}static/'}
-c.NotebookApp.default_url = "/lab/tree/mnt"
-print(c.NotebookApp.default_url)
+c.NotebookApp.default_url = '/lab/tree/mnt'
 c.NotebookApp.token = u''
 EOF
-echo "c.NotebookApp.default_url after ipython_notebook_config.py written"
-echo "/lab/tree/mnt"
+
 COMMAND='jupyter-lab --config="$CONF_FILE" --no-browser --ip="0.0.0.0" 2>&1'
-eval ${COMMAND}                                                                                                              
+eval ${COMMAND} 
