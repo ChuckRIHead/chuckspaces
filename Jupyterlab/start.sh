df53abdde3d51e7b4b524fd5cb8f9170b4bb16ea
ChuckRIHead <chuckhead@gmail.com>
2018-05-22 14:01:18 -0700

    Update start.sh

--- Jupyterlab/start.sh
+++ Jupyterlab/start.sh
@@ -9,13 +9,14 @@ PREFIX=/${DOMINO_PROJECT_OWNER}/${DOMINO_PROJECT_NAME}/notebookSession/${DOMINO_
 
 cat >> $CONF_FILE << EOF
 c = get_config()
-c.NotebookApp.notebook_dir = '${DOMINO_WORKING_DIR:-"/"}'
+c.NotebookApp.notebook_dir = '${DRT_WORKING_DIR:-"/"}'
 c.NotebookApp.base_url = '${PREFIX}'
 c.NotebookApp.base_kernel_url = '${PREFIX}'
 c.NotebookApp.base_project_url = '${PREFIX}'
 c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}, 'static_url_prefix': '${PREFIX}static/'}
+c.NotebookApp.default_url = "/lab/tree/$DOMINO_WORKING_DIR"
 c.NotebookApp.token = u''
 EOF
-                                                                                                                                    
+
 COMMAND='jupyter-lab --config="$CONF_FILE" --no-browser --ip="0.0.0.0" 2>&1'
-eval ${COMMAND}                                                                                                               
+eval ${COMMAND}                                                                                                              
